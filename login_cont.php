db561967c1f365532aab3aad7ddbf1c280ba140f
SABINO KARI B <202100977b@utea.edu.pe>
2023-11-15 19:43:12 -0500

    Proyecto_final_login1

--- login_cont.php
+++ login_cont.php
@@ -3,6 +3,8 @@ session_start();
 
 if (!empty($_POST["btningresar"])) {
     //echo "Boton precionado";
+    //echo "Boton precionado";
+    //echo "Boton precionado";
     if (!empty($_POST["usuario"]) and !empty($_POST["password"])) {
         $usuario = $_POST["usuario"];
         $password = $_POST["password"];
